dba0b4bb012adfd4ead0b1c57ca15bfa4507daa7
FreyrsKept <120368406+FreyrsKept@users.noreply.github.com>
2023-11-17 21:09:51 +0700

    P12

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import * as auth from "../utils/auth";
 import api from '../utils/Api';
 import Header from './Header';
 import Main from './Main';
@@ -8,14 +10,26 @@ import AddPlacePopup from './AddPlacePopup';
 import ImagePopup from "./ImagePopup";
 import { CurrentUserContext } from "./CurrentUserContext";
 import EditAvatarPopup from './EditAvatarPopup';
+import InfoTooltip from './InfoTooltip';
+import ProtectedRoute from './ProtectedRoute';
+import Login from './Login';
+import Register from './Register';
+import resolve from "../images/resolve.svg";
+import reject from "../images/reject.svg";
 
 function App() {
-  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
-  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = React.useState(false);
-  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
-  const [selectedCard, setSelectedCard] = React.useState(null);
-  const [currentUser, setCurrentUser] = React.useState({});
-  const [cards, setCards] = React.useState([]);
+  const navigate = useNavigate();
+  const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
+  const [isAddPlacePopupOpen, setIsAddPlacePopupOpen] = useState(false);
+  const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = useState(false);
+  const [selectedCard, setSelectedCard] = useState(null);
+  const [currentUser, setCurrentUser] = useState({});
+  const [cards, setCards] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [emailName, setEmailName] = useState(null);
+  const [popupImage, setPopupImage] = useState("");
+  const [popupTitle, setPopupTitle] = useState("");
+  const [infoTooltip, setInfoTooltip] = useState(false);
 
 
   useEffect(() => {
@@ -42,6 +56,26 @@ function App() {
       }
     }
   }, [isAddPlacePopupOpen, isEditAvatarPopupOpen, isEditProfilePopupOpen, selectedCard]);
+
+  useEffect(() => {
+    const jwt = localStorage.getItem("jwt");
+    if (jwt) {
+      auth.getToken(jwt).then((res) => {
+        if (res) {
+          setIsLoggedIn(true);
+          setEmailName(res.data.email);
+        }
+      }).catch((err) => {
+        console.log(err);
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isLoggedIn === true) {
+      navigate("/");
+    }
+  }, [isLoggedIn, navigate]);
 
   function handleCardLike(card) {
     const isLiked = card.likes.some((i) => i._id === currentUser._id);
@@ -95,6 +129,30 @@ function App() {
     })
   }
 
+  function onRegister(email, password) {
+    auth.registerUser(email, password).then(() => {
+      setPopupImage(resolve);
+      setPopupTitle("Вы успешно зарегистрировались!");
+      navigate("/sign-in");
+    }).catch(() => {
+      setPopupImage(reject);
+      setPopupTitle("Что-то пошло не так! Попробуйте ещё раз.");
+    }).finally(handleInfoTooltip);
+  }
+
+  function onLogin(email, password) {
+    auth.loginUser(email, password).then((res) => {
+      localStorage.setItem("jwt", res.token);
+      setIsLoggedIn(true);
+      setEmailName(email);
+      navigate("/");
+    }).catch(() => {
+      setPopupImage(reject);
+      setPopupTitle("Что-то пошло не так! Попробуйте ещё раз.");
+      handleInfoTooltip();
+    });
+  }
+
   function handleAvatarEdit() {
     setIsEditAvatarPopupOpen(true);
   }
@@ -111,6 +169,10 @@ function App() {
     setSelectedCard(card);
   }
 
+  function handleInfoTooltip() {
+    setInfoTooltip(true);
+  }
+
   function closePopups() {
     setIsEditAvatarPopupOpen(false);
     setIsEditProfilePopupOpen(false);
@@ -118,23 +180,53 @@ function App() {
     setSelectedCard(null);
   }
 
+  function onSignOut() {
+    setIsLoggedIn(false);
+    setEmailName(null);
+    navigate("/sign-in");
+    localStorage.removeItem("jwt")
+  }
+
   return (
     <CurrentUserContext.Provider value={currentUser}>
       <div className='page'>
         <div className='page__content'>
-          <Header />
+          <Routes>
+            <Route path="sign-in" element={
+              <>
+                <Header title="Регистрация" route="sign-in" />
+                <Login onLogin={onLogin} />
+              </>
+            } />
 
-          <Main
-            onEditAvatar={handleAvatarEdit}
-            onEditProfile={handleProfileEdit}
-            onAddPlace={handleAddPlace}
-            onCardClick={handleCardClick}
-            cards={cards}
-            onCardLike={handleCardLike}
-            onCardDelete={handleCardDelete}
-          />
+            <Route path="/sing-up" element={
+              <>
+                <Header title="Войти" route="/sign-in" />
+                <Register onRegister={onRegister} />
+              </>
+            } />
 
-          <Footer />
+            <Route exact path="/" element={
+              <>
+                <Header title="Выйти" mail={emailName} onClick={onSignOut} route="" />
+                <ProtectedRoute
+                  component={Main}
+                  isLogged={isLoggedIn}
+                  onEditAvatar={handleAvatarEdit}
+                  onEditProfile={handleProfileEdit}
+                  onAddPlace={handleAddPlace}
+                  onCardClick={handleCardClick}
+                  cards={cards}
+                  onCardLike={handleCardLike}
+                  onCardDelete={handleCardDelete}
+                />
+                <Footer />
+              </>
+            } />
+
+            <Route path="*" element={<Navigate to={isLoggedIn ? "/" : "/sign-in"} />} />
+          </Routes>
+
 
           <EditProfilePopup
             isOpen={isEditProfilePopupOpen}
@@ -156,6 +248,13 @@ function App() {
 
           <ImagePopup
             card={selectedCard}
+            onClose={closePopups}
+          />
+
+          <InfoTooltip
+            image={popupImage}
+            title={popupTitle}
+            isOpen={infoTooltip}
             onClose={closePopups}
           />
         </div>
---
+++ src/components/Register.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Link } from "react";
+
+function Register(props) {
+  const [email, setEmail] = useState("")
+  const [passsword, setPassword] = useState("")
+
+  function handleEmailInput(evt) {
+    setEmail(evt.target.value);
+  }
+  function handlePasswordInput(evt) {
+    setPassword(evt.target.value)
+  }
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    props.onRegister(email, passsword);
+  }
+
+  return (
+    <section className="login">
+      <h2 className="login__title">Регистрация</h2>
+      <form className="login__form" onSubmit={handleSubmit}>
+        <input className="login__input" type="email" placeholder="Email" value={email} onChange={handleEmailInput} required></input>
+        <input className="login__input" type="password" placeholder="Пароль" value={passsword} onChange={handlePasswordInput} autoComplete="on" required></input>
+        <button className="login__button" type="submit">Зарегистрироваться</button>
+      </form>
+      <p className="login__sign-in">Уже зарегистрированы?<Link to="/sign-in" className="login__link">Войти</Link> </p>
+    </section>
+  )
+}
+
+export default Register;
